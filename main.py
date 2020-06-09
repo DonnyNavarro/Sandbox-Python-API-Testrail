c7ac0ec5d4a709a8232b5c92cc962b9cf4f78caf
DonnyNavarro <donny@ultratesting.us>
2020-06-08 23:01:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,32 +9,32 @@ import requests
 import json
 
 class apiRequest(object):
-    def __init__(self):
+    def __init__(self, params):
         self.url = os.getenv("TEST_RAIL_BASE_URL")
         self.username = os.getenv("TEST_RAIL_USERNAME")
         self.password = os.getenv("TEST_RAIL_API_KEY") # or password
         self.headers = {
             'Content-type': 'application/json'
             }
-        self.params = "/get_plan/52314"
-        self.testrunId = "52314"
+        self.planId = "/52314"
+        self.params = params
 
-    def requestPlan(self, testrunId=None):
-        """Get Test Plan details for the Testplan of the testrunId"""
-        if not testrunId:
-            testrunId = self.testrunId
-        params = "/get_plan/"
-        data = self.sendRequest(params+testrunId)
-        return data
+    # def requestPlan(self, testrunId=None):
+    #     """Get Test Plan details for the Testplan of the testrunId"""
+    #     if not testrunId:
+    #         testrunId = self.testrunId
+    #     params = "/get_plan/"
+    #     data = self.sendRequest(params+testrunId)
+    #     return data
 
-    def sendRequest(self, params):
+    def sendRequest(self):
         """Session Example: Use session to send api requests with auth parameters"""
         session = requests.Session()
         session.auth = (self.username, self.password)
-        auth = session.post(self.url)
-        
+        auth = session.post(self.url+self.params)
+
         print("Sending request",self.url+self.params+"...")
-        response = session.get(self.url + self.params, headers=self.headers)
+        response = session.get(self.url + self.params + self.planId, headers=self.headers)
         print("Response Status Code:",response.status_code)
 
         # Validate request status
@@ -50,6 +50,7 @@ def displayJson(jsonData):
     print(json.dumps(jsonDict, indent=4))
     # end basic session
 
-testrail = apiRequest()
-planDetail = testrail.requestPlan()
+
+getPlan = apiRequest("/get_plan")
+planDetail = getPlan.sendRequest()
 displayJson(planDetail) if planDetail else False
