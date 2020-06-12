07df8b121a736d76f823fcf981a88c6ad6b6bba6
DonnyNavarro <donny@ultratesting.us>
2020-06-12 14:17:10 -0400

    Update bb.py

--- bb.py
+++ bb.py
@@ -1,18 +1,22 @@
-"""External ENV file support (API KEY)"""
-#   Then we can store secure environmentals in the .env file, and grab them with os.getenv("varname")
 import os
+import requests
+import json
 from dotenv import load_dotenv
 project_folder = os.path.expanduser('') # local path
 load_dotenv(os.path.join(project_folder, '.env'))
 
-import requests
-import json
-from datetime import datetime
-import time
+"""Testrail Reporting via API"""
+# testrailRequest class packages API requests, including saving data internally, and various other functions
 
-class apiRequest(object):
+# prompt functions package the user input handling and validation depending on the type of answer expected
+
+# reportResult function is a placeholder illustration of how to send results after the script has been completed
+
+# __main__ workflow is a commandline user experience designed to ask the user for their preferences and automatically create anything needed in Testrail in preparation for being able to use the reportResult function to send results to Testrail
+
+class testrailRequest(object):
     def __init__(self, requestType, urlParams, payload={}, files={}):
-        """Create an api request instance. 
+        """Create a Testrail API request instance. Note that this doesnt send the request until self.sendRequest() is run
         
         requestType is str "get", "post" etc. 
         urlParams is the tail of the url
@@ -25,11 +29,7 @@ class apiRequest(object):
         self.urlParams = urlParams
         self.headers = {'Content-type': 'application/json'}
         self.payload = payload
-        self.files = files
         self.response = False
-
-        if files: #Change content type when attaching files
-            self.headers = {'Content-type': 'multipart/form-data'}
 
     def sendRequest(self):
         """Send the API to Testrail defined in in this instance"""
@@ -42,8 +42,7 @@ class apiRequest(object):
             self.requestType,
             self.baseUrl+self.urlParams, 
             headers=self.headers, 
-            json=self.payload,
-            files=self.files
+            json=self.payload
             )
         print("  ...Response Status Code:",self.response.status_code)
 
@@ -69,18 +68,6 @@ class apiRequest(object):
             return False
         print("  ...Response Reason:",self.response.reason)
         print(json.dumps(self.response.json(), indent=4))
-
-    def responseExport(self):
-        """Export the response as a JSON to a local folder"""
-        if not self.checkResponse():
-            return False
-        time = datetime.today().strftime('%Y-%m-%d-%H%M%S')
-        # Save the log file
-        with open("logs/"+time+".json", "w") as outfile:
-            json.dump(self.response.json(), outfile, indent=4)
-
-def nowTime():
-    return datetime.today().strftime('%Y-%m-%d-%H%M%S')
 
 def promptYesno(message):
     """Prompt the user with the message and only accept yes/no as input. Return true if they say yes."""
@@ -118,89 +105,9 @@ def promptText(message):
             print("ERROR: Input not recognized. Choose text\n")
             choice = ""
     return choice
-    
-if __name__ == '__main__':
-    projectNum = 0
-    suiteNum = 0
-    milestoneNum = 0
-    milestoneName = ""
-    milestoneResponse = False
-    testsList = {}
 
-    testrunReuse = promptYesno("Would you like to use a previous Testrun?")
-    if testrunReuse:
-        # Reuse a Testrun ID!
-        testrunNum = promptNum("Testrun ID #")
-        testrunResponse = apiRequest("get", "/get_run/"+testrunNum)
-        testrunResponse.sendRequest()
-        # testrunResponse.responseDisplay()
-        projectNum = testrunResponse.response.json()["project_id"] #jic
-        milestoneNum = testrunResponse.response.json()["milestone_id"] #jic
-        suiteNum = testrunResponse.response.json()["suite_id"] #jic
-    else:
-        # Creating a new Testrun! But first check if we want Milestones!
-        milestone = promptYesno("Would you like to use a Milestone at all?")
-        if milestone:
-            # Use a Milestone!
-            milestoneReuse = promptYesno("Would you like to use a previous Milestone?")
-            if milestoneReuse:
-                # Reuse a Milestone ID!
-                milestoneNum = promptNum("Milestone ID #")
-                milestoneResponse = apiRequest("get", "/get_milestone/"+milestoneNum)
-                milestoneResponse.sendRequest()
-                # milestoneResponse.responseDisplay()
-                milestoneResponse.name = milestoneResponse.response.json()["name"]
-                print("Milestone:",milestoneResponse.name)
-                milestoneResponse.id = milestoneResponse.response.json()["id"]
-                projectNum = milestoneResponse.response.json()["project_id"] #jic
-            else:
-                # Create a new Milestone!
-                projectNum = promptNum("What is the Testrail Project Number?") # this should usually be replaced by an environmental
-                milestoneName = promptText("What would you like to name the new Milestone?")
-                milestoneResponse = apiRequest("post", "/add_milestone/"+str(projectNum), {"name": milestoneName})
-                milestoneResponse.sendRequest()
-                # milestoneResponse.responseDisplay()
-                milestoneResponse.id = milestoneResponse.response.json()["id"]
-                milestoneNum = milestoneResponse.response.json()["id"]
-
-        # Create a new Testrun!
-        testrunName = promptText("What would you like to name the new Testrun?")
-        suiteNum = promptNum("Test Suite ID #") if not suiteNum else suiteNum
-        projectNum = promptNum("Testrail Project #") if not projectNum else projectNum
-        testrunPayload = {
-            "suite_id": suiteNum,
-            "name": testrunName
-        }
-        if milestone:
-            testrunPayload["milestone_id"] = milestoneNum
-        testrunResponse = apiRequest("post", "/add_run/"+str(projectNum), testrunPayload)
-        testrunResponse.sendRequest()
-        # testrunResponse.responseDisplay()
-        testrunResponse.id = testrunResponse.response.json()["id"]
-        testrunNum = testrunResponse.response.json()["id"]
-        projectNum = testrunResponse.response.json()["project_id"] # jic
-        milestoneNum = testrunResponse.response.json()["milestone_id"] #jic
-        suiteNum = testrunResponse.response.json()["suite_id"] #jic
-
-    # Now that we have our Testrun defined
-    # Get test IDs and store them in testsList dict
-    testsResponse = apiRequest("get", "/get_tests/"+str(testrunNum))
-    testsResponse.sendRequest()
-    # testsResponse.responseDisplay()
-    for test in testsResponse.response.json():
-        # Organize tests by title so specific ones can have their ids referenced
-        testsList[test["title"]] = {
-            "id": test["id"],
-            "case_id": test["case_id"]
-        }
-    """ FINAL RESULT: testsList dict of test IDs to send results to """
-    # At this point we should have a set of results previously prepared that we want to send
-    # Results to report will need a test name to correspond to the TC name in Testrail
-    # By matching the name we can get the testsList[name]["id"] in order to send results there
-    print(json.dumps(testsList, indent=4))
-
-def reportResult(testname, testsList, status, comment):
-    """Report a result for testname to Testrail, based on ID mapping in the testsList
+def reportResult(testname, testsList, status, comment=False):
+    """Report a result for testname to Testrail, based on test ID mapping in the testsList
     
     testname: Should match the name of the testcase in testrail
     testsList: a dict of testnames to testrail id numbers
@@ -217,9 +124,90 @@ def reportResult(testname, testsList, status, comment):
         "fail": 5
     }
     payload = {
-        "status_id": statusMap[status],
-        "comment": comment
+        "status_id": statusMap[status]
     }
-    addResult = apiRequest("post", "/add_result/"+str(testsList[testname]["id"]), payload)
+    if comment:
+        payload["comment"] = comment
+    addResult = testrailRequest("post", "/add_result/"+str(testsList[testname]["id"]), payload)
     addResult.sendRequest()
+    
+if __name__ == '__main__':
+    projectNum = 0
+    suiteNum = 0
+    milestoneNum = 0
+    milestoneName = ""
+    milestoneResponse = False
+    testsList = {}
 
+    testrunReuse = promptYesno("Would you like to use a previous Testrun?")
+    if testrunReuse:
+        # Reuse a Testrun ID!
+        testrunNum = promptNum("Testrun ID #")
+        testrunResponse = testrailRequest("get", "/get_run/"+testrunNum)
+        testrunResponse.sendRequest()
+        # testrunResponse.responseDisplay()
+        projectNum = testrunResponse.response.json()["project_id"] #jic
+        milestoneNum = testrunResponse.response.json()["milestone_id"] #jic
+        suiteNum = testrunResponse.response.json()["suite_id"] #jic
+    else:
+        # Creating a new Testrun! But first check if we want Milestones!
+        milestone = promptYesno("Would you like to use a Milestone at all?")
+        if milestone:
+            # Use a Milestone!
+            milestoneReuse = promptYesno("Would you like to use a previous Milestone?")
+            if milestoneReuse:
+                # Reuse a Milestone ID!
+                milestoneNum = promptNum("Milestone ID #")
+                milestoneResponse = testrailRequest("get", "/get_milestone/"+milestoneNum)
+                milestoneResponse.sendRequest()
+                # milestoneResponse.responseDisplay()
+                milestoneResponse.name = milestoneResponse.response.json()["name"]
+                print("Milestone:",milestoneResponse.name)
+                milestoneResponse.id = milestoneResponse.response.json()["id"]
+                projectNum = milestoneResponse.response.json()["project_id"] #jic
+            else:
+                # Create a new Milestone!
+                projectNum = promptNum("What is the Testrail Project Number?") # this should usually be replaced by an environmental
+                milestoneName = promptText("What would you like to name the new Milestone?")
+                milestoneResponse = testrailRequest("post", "/add_milestone/"+str(projectNum), {"name": milestoneName})
+                milestoneResponse.sendRequest()
+                # milestoneResponse.responseDisplay()
+                milestoneResponse.id = milestoneResponse.response.json()["id"]
+                milestoneNum = milestoneResponse.response.json()["id"]
+
+        # Create a new Testrun!
+        testrunName = promptText("What would you like to name the new Testrun?")
+        suiteNum = promptNum("Test Suite ID #") if not suiteNum else suiteNum
+        projectNum = promptNum("Testrail Project #") if not projectNum else projectNum
+        testrunPayload = {
+            "suite_id": suiteNum,
+            "name": testrunName
+        }
+        if milestone:
+            testrunPayload["milestone_id"] = milestoneNum
+        testrunResponse = testrailRequest("post", "/add_run/"+str(projectNum), testrunPayload)
+        testrunResponse.sendRequest()
+        # testrunResponse.responseDisplay()
+        testrunResponse.id = testrunResponse.response.json()["id"]
+        testrunNum = testrunResponse.response.json()["id"]
+        projectNum = testrunResponse.response.json()["project_id"] # jic
+        milestoneNum = testrunResponse.response.json()["milestone_id"] #jic
+        suiteNum = testrunResponse.response.json()["suite_id"] #jic
+
+    # Now that we have our Testrun defined
+    # Get test IDs and store them in testsList dict
+    testsResponse = testrailRequest("get", "/get_tests/"+str(testrunNum))
+    testsResponse.sendRequest()
+    # testsResponse.responseDisplay()
+    for test in testsResponse.response.json():
+        # Organize tests by title so specific ones can have their ids referenced
+        testsList[test["title"]] = {
+            "id": test["id"],
+            "case_id": test["case_id"]
+        }
+    """ FINAL RESULT: testsList dict of test IDs to send results to """
+    print(json.dumps(testsList, indent=4))
+    print("At this point we should have a set of results previously prepared that we want to send")
+    print("Results to report will need a test name to correspond to the TC name in Testrail")
+    print("By matching the name we can get the testsList[name][\"id\"] in order to send results there")
+    
